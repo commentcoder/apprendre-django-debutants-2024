8034e9b9ce13a8a290706d2a6adfc160dbcf82f5
Thomas Collart <thomas.collart@gmail.com>
2024-06-01 13:57:31 +0300

    Init partie_1_introduction

--- feed/views.py
+++ feed/views.py
@@ -1,15 +1,1 @@
-from datetime import datetime
 from django.shortcuts import render
-
-def index(request):
-    context = {
-        'messages': [
-            {
-                'content': 'text',
-                'username': 'CommentCoder',
-                'created_at': datetime.now()
-            }
-        ]
-    }
-
-    return render(request, 'index.html', context=context)
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from feed.views import index
 
 urlpatterns = [
-    path('', index),
     path('admin/', admin.site.urls),
 ]
