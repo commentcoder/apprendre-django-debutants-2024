3bf9e57fdbb6fae5317031c888094f406311e718
Thomas Collart <thomas.collart@gmail.com>
2024-06-03 20:11:09 +0300

    Add partie 4 modeles

--- feed/admin.py
+++ feed/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+from feed.models import Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['content', 'user']
+
+
+admin.site.register(Message, MessageAdmin)
