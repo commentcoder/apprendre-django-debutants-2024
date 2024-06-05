417bf195548e3b4a47899f58dc8f3390b87f22b5
Thomas Collart <thomas.collart@gmail.com>
2024-06-05 08:53:03 +0300

    Partie 5 orm

--- feed/views.py
+++ feed/views.py
@@ -1,25 +1,9 @@
-from datetime import datetime
 from django.shortcuts import render
+from feed.models import Message
 
 def index(request):
-    context = {
-        'messages': [
-            {
-                'content': 'text',
-                'username': 'CommentCoder',
-                'created_at': datetime.now()
-            },
-            {
-                'content': 'text',
-                'username': 'CommentCoder',
-                'created_at': datetime.now()
-            },
-            {
-                'content': 'text',
-                'username': 'CommentCoder',
-                'created_at': datetime.now()
-            }
-        ]
-    }
+    context = {}
+
+    context['messages'] = Message.objects.order_by('-created_at')
 
     return render(request, 'index.html', context=context)
