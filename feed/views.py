4d0fe830a20fd51119a24250194c6a3284ccd48e
Thomas Collart <thomas.collart@gmail.com>
2024-06-05 08:54:04 +0300

    Partie 5

--- feed/views.py
+++ feed/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from feed.models import Message
 
 def index(request):
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        user = request.user
+
+        Message.objects.create(content=content, user=user)
+
     context = {}
 
     context['messages'] = Message.objects.order_by('-created_at')
