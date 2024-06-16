4d862d9e35d23946828c48bfffffc5af49d4b5fa
Thomas Collart <thomas.collart@gmail.com>
2024-06-16 13:23:28 +0300

    Partie 8

--- feed/views.py
+++ feed/views.py
@@ -17,10 +17,14 @@ def index(request):
 
 
 def details(request, id):
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        user = request.user
+
+        Message.objects.create(content=content, user=user, response_to_id=id)
+
     context = {}
     context['message'] = Message.objects.get(id=id)
     context['comments'] = Message.objects.filter(response_to=id)
-
-    print(context)
 
     return render(request, 'details.html', context=context)
